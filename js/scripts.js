e4ae6a6152e34e1205bb787bda36b1404e185ee1
mcaprari <caprarimilan@gmail.com>
2023-01-03 19:53:10 -0500

    Task 1.2
    First practice using arrays in JavaScript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-alert('Hello world');
- let favoriteFood = pizza;
- document.write(favoriteFood);
-let name = Jerry
+let pokemonList = [
+    { name: "Charmander", height: 9, type: 'fire'},
+    { name: "Pikachu", height: 6, type: 'electric'},
+    { name: "Squirtle", height: 7, type: 'water'}
+];
 
 
- 
