f9c728e2ca1b0d5162effcaaeca2304bd4897492
mcaprari <caprarimilan@gmail.com>
2023-01-09 18:21:51 -0500

    Task 1.2 version 2
    Correction with array of strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-    { name: "Charmander", height: 9, type: 'fire'},
-    { name: "Pikachu", height: 6, type: 'electric'},
-    { name: "Squirtle", height: 7, type: 'water'}
+    { name: "Charmander", height: 9, type: ['fire', 'flying', 'normal']},
+    { name: "Pikachu", height: 6, type: ['electric', 'fighting', 'normal']},
+    { name: "Squirtle", height: 7, type: ['water', 'ice', 'normal']}
 ];
 
 
