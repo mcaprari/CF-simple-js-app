4b070fbe66048eef4d3c0b501b69f2806c9cab70
mcaprari <caprarimilan@gmail.com>
2023-01-13 17:37:54 -0500

    Corrections and additions
    -Made corrections to pokemon's height
    -added all evolutions of each chosen pokemon
    -made other minor corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 let pokemonList = [
-    { name: "Charmander", height: 9, type: ['fire', 'flying', 'normal']},
-    { name: "Pikachu", height: 6, type: ['electric', 'fighting', 'normal']},
-    { name: "Squirtle", height: 7, type: ['water', 'ice', 'normal']}
+    {name: "Charmander", height: .6,type: ['fire', 'flying']},
+    {name: "Charmeleon", height: 1.1, type: ['fire', 'flying']},
+    {name: "Charizard", heihgt: 1.7, type: ['fire', 'flying']},
+    {name: "Pikachu", height: .4, type: 'electric'},
+    {name: "Raichu", height: .8, type: 'electric'},
+    {name: "Squirtle", height: .5, type: 'water'},
+    {name: "Wartortle", height:1, type: 'water'},
+    {name: "Blastoise", height: 1.6, type: 'water'},
 ];
-
 
