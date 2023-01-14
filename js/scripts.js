baab2253bece7f9d716fcc0ce866da33c0c8372b
mcaprari <caprarimilan@gmail.com>
2023-01-14 17:09:10 -0500

    corrected grammar error
    corrected a misspelling

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
     {name: "Charmander", height: .6,type: ['fire', 'flying']},
     {name: "Charmeleon", height: 1.1, type: ['fire', 'flying']},
-    {name: "Charizard", heihgt: 1.7, type: ['fire', 'flying']},
+    {name: "Charizard", height: 1.7, type: ['fire', 'flying']},
     {name: "Pikachu", height: .4, type: 'electric'},
     {name: "Raichu", height: .8, type: 'electric'},
     {name: "Squirtle", height: .5, type: 'water'},
@@ -10,7 +10,7 @@ let pokemonList = [
 ];
 
 for (let i=0; i < pokemonList.length;i++) { 
-    if (pokemonList[i].height >= 1.1){
+    if (pokemonList[i].height >= 1){
         console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!" + "<br>");
         document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
     
